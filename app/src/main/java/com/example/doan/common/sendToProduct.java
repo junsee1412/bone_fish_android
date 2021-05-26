ded3b0aa69e661e99e4ef81515744f58fe767751
junsee <dat10319@gmail.com>
2021-05-27 02:18:42 +0700

    update layout login

--- app/src/main/java/com/example/doan/common/sendToProduct.java
+++
@@ -1,9 +0,0 @@
-package com.example.doan.common;
-
-import com.example.doan.model.Product;
-
-import java.util.List;
-
-public interface sendToProduct {
-    void setData(List<Product> productList);
-}
